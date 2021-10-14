9494c75e22cf327278d71414b40cbaf9bebb246c
Ashwin Rangarajan <r@ashwin.cloud>
2021-10-13 23:31:12 -0500

    Add solution to Count Primes

---
+++ Math/CountPrimes/problem.py
@@ -0,0 +1,3 @@
+# PROBLEM STATEMENT
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/744/
+# Given an integer n, return the number of prime numbers that are strictly less than n.
