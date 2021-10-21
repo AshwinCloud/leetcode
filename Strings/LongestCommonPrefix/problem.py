a169f2f9c1ea9ec8393b2451ffaef80d385db6ee
Ashwin Rangarajan <r@ashwin.cloud>
2021-10-20 22:40:46 -0500

    Add solution to Longest Common Prefix

---
+++ Strings/LongestCommonPrefix/problem.py
@@ -0,0 +1,4 @@
+# PROBLEM STATEMENT
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/887/
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
