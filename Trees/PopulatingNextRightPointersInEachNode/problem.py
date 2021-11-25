c0f3ce2e5e8bd9d4ec980fbd702831a1d7465d9b
Ashwin Rangarajan <r@ashwin.cloud>
2021-11-24 23:55:00 -0600

    Add solution to Populating Next Right Pointers in Each Node

---
+++ Trees/PopulatingNextRightPointersInEachNode/problem.py
@@ -0,0 +1,4 @@
+# PROBLEM STATEMENT
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+# You are given a perfect binary tree where all leaves are on the same level, and every parent has two children.
+# The binary tree has the following definition:
