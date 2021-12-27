49408940dc752d5e60330f6b491a25c4765cd9c3
Ashwin Rangarajan <r@ashwin.cloud>
2021-12-26 19:09:07 -0600

    Add solution to Longest Common Subsequence

---
+++ DynamicProgramming/LongestCommonSubsequence/solution.py
@@ -0,0 +1,41 @@
+# PROBLEM STATEMENT
+# https://leetcode.com/problems/longest-common-subsequence/
+# Given two strings text1 and text2, return the length of their longest common subsequence.
+# If there is no common subsequence, return 0.
+# A subsequence of a string is a new string generated from the original string with some characters
+# (can be none) deleted without changing the relative order of the remaining characters.
+# For example, "ace" is a subsequence of "abcde".
+# A common subsequence of two strings is a subsequence that is common to both strings.
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return self.longestCommonSubsequenceDP(text1, text2)
+
+    # Time Complexity: O(n*n)
+    # Space Complexity: O(n)
+    def longestCommonSubsequenceDP1(self, text1: str, text2: str) -> int:
+        prev_row = [0] * (len(text2) + 1)
+
+        for i in range(len(text1) - 1, -1, -1):
+            curr_row = [0] * (len(text2) + 1)
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    curr_row[j] = 1 + prev_row[j + 1]
+                else:
+                    curr_row[j] = max(curr_row[j + 1], prev_row[j])
+            prev_row = curr_row
+
+        return prev_row[0]
+
+    # Time Complexity: O(n*n)
+    # Space Complexity: O(n*n)
+    def longestCommonSubsequenceDP2(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text2) + 1) for j in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return dp[0][0]
