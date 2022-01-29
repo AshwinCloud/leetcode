0834507db51a794eb1c3f5128e3357317b4fc8c5
Ashwin Rangarajan <r@ashwin.cloud>
2022-01-28 23:48:26 -0600

    Add solution to Is Subsequence

---
+++ Strings/IsSubsequence/solution.py
@@ -0,0 +1,23 @@
+# PROBLEM STATEMENT
+# https://leetcode.com/problems/is-subsequence/
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+# A subsequence of a string is a new string that is formed from the original string by deleting some
+# (can be none) of the characters without disturbing the relative positions of the remaining characters.
+# (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        return self.isSubsequence1(s, t)
+
+    # Time Complexity: O(n) where n is the size if t
+    # Space Complexity: O(1)
+    def isSubsequence1(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        return i == len(s)
