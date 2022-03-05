08a869975012e7cbfbd2af24ee3dec653d075aa7
Ashwin Rangarajan <r@ashwin.cloud>
2022-03-04 22:50:25 -0600

    Update solution for ContainsDuplicate

--- Array/ContainsDuplicate/problem.py
+++
@@ -1,3 +0,0 @@
-# PROBLEM STATEMENT
-# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
-# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
--- Array/ContainsDuplicate/solution.py
+++ Array/ContainsDuplicate/solution.py
@@ -3,24 +3,20 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return self.containsDuplicateSort(nums)
-        
-    def containsDuplicateDictionary(self, nums: List[int]) -> bool:
-        dictionary = {}
-        
-        for n in nums:
-            if dictionary.get(n):
-                return True
-            else:
-                dictionary[n] = True
-        
-        return False
-    
+        return self.containsDuplicateSet(nums)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def containsDuplicateSet(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+
+    # Time Complexity: O(n log n)
+    # Space Complexity: O(1)
     def containsDuplicateSort(self, nums: List[int]) -> bool:
         nums.sort()
-        
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
                 return True
-        
+
         return False
